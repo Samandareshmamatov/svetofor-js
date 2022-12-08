dbaa8afb0770a236f4fdeede5f1318fa7b3bc23e
Samandareshmamatov <samandareshmamatov1127@gmail.com>
2022-12-08 16:20:04 +0500

    Add this project

---
+++ main.js
@@ -0,0 +1,22 @@
+const redCircle = document.getElementById("redCircle");
+const yellowCircle = document.getElementById("yellowCircle")
+const greenCircle = document.getElementById("greenCircle")
+
+let count = 1;
+setInterval(() => {
+    if (count % 7 == 1 || count % 7 == 2 || count % 7 == 3) {
+        redCircle.style.backgroundColor = "red"
+        greenCircle.style.backgroundColor = "#585858"
+        yellowCircle.style.backgroundColor = "#585858"
+    } else if (count % 7 == 4) {
+        redCircle.style.backgroundColor = "#585858"
+        greenCircle.style.backgroundColor = "#585858"
+        yellowCircle.style.backgroundColor = "yellow"
+    } else if (count % 7 == 5 || count % 7 == 6 || count % 7 == 0) {
+        redCircle.style.backgroundColor = "#585858"
+        greenCircle.style.backgroundColor = "#00ff00"
+        yellowCircle.style.backgroundColor = "#585858"
+        count = 1;
+    }
+    count++;
+}, 1000)
